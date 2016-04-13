03945bf0dbc4cb9df8fc612553e189288aa1dc7b
jinga <ilovejinjoo@gmail.com>
2016-04-14 02:00:34 +0900

    codejam2

--- 2015/codejam2.cpp
+++ 2015/codejam2.cpp
@@ -7,9 +7,10 @@
 
 using namespace std;
 
-int main() {
+int start(const string& input_file_name, const string& output_file_name) {
 
-	std::ifstream input("problem2.in.small.txt");
+	std::ifstream input(input_file_name);
+	std::ofstream output(output_file_name);
 
 	int T;
 	input >> T;
@@ -48,56 +49,94 @@ int main() {
 		}
 
 		if (N == K || N < K) {
-			cout << "NO" << endl;
-			continue;
+			goto PRINT_NO;			
 		}
 
+		// order by y
 		sort(points.begin(), points.end(), [](const vector<long>& a, const vector<long>& b){ return a[1] < b[1]; });
 
-		long top_count = 1, bottom_count = 1, top_y = points[N-K][1], bottom_y = points[K][1], bottom_result = 0, top_result = 0;
-		for (int j = K+1; j < N - K - 1 ; j++) { // 남북 성벽 흔적의 갯수를 알 수 없으므로 우선 남북으로 K개 만큼 빼고 평균치를 구해서 남/북성벽 Y값을 Estimation
-			if (points[j][1] <= points[K][1] + W) {
-				bottom_y += points[j][1];
-				bottom_count++;
+		int bottom_min = 0, bottom_max = 0, top_min = 0, top_max = 0;
+
+		// find index of top of the bottom		
+		if (K <= (N / 2)) {
+			if (points[K][1] + W < points[N / 2][1]) {
+				for (int j = N / 2; j >= K; j--) {
+					if (points[j][1] <= points[K][1] + W) {
+						bottom_max = j;
+						top_min = j + 1;
+						break;
+					}
+				}
 			}
 			else {
-				top_y += points[j][1];
-				top_count++;
+				for (int j = N / 2; j <= N; j++) {
+					if (points[j][1] > points[K][1] + W) {
+						bottom_max = j - 1;
+						top_min = j;
+						break;
+					}
+				}
 			}
 		}
+		else {
+			// K를 뺀 나머지 N의 개수가 직사각형이 되므로 무조건 YES
+			goto PRINT_YES;			
+		}
 
-		bottom_result = bottom_y / bottom_count;
-		top_result = top_y / top_count;
-
-		long bottom_min = 0;
-		for (int j = K; j > 0; j--) {
-			if (points[j][1] >= bottom_result - W) {
-				bottom_y += points[j][1];
-				bottom_min++;
+		// find first index of the bottom		
+		if(bottom_max > 0) {
+			for (int j = bottom_max; j > 0 ; j--) { // 남북 성벽 흔적의 갯수를 알 수 없으므로 우선 남북으로 K개 만큼 빼고 평균치를 구해서 남/북성벽 Y값을 Estimation
+				if (points[j][1] < points[bottom_max][1] - W) {
+					bottom_min = j + 1;
+					break;
+				}
 			}
 		}
-		bottom_result = bottom_y / (bottom_count + bottom_min); // bottom 평균
+		else {
+			bottom_min = bottom_max;
+		}
 
-		int top_max = 0;
-		for (int j = N-K; j < N; j++) {
-			if (points[j][1] <= top_result + W) {
-				top_y += points[j][1];
-				top_max++;
+		// find first index of the top
+		if (top_min < N) {
+			for (int j = top_min; j < N; j++) { // 남북 성벽 흔적의 갯수를 알 수 없으므로 우선 남북으로 K개 만큼 빼고 평균치를 구해서 남/북성벽 Y값을 Estimation
+				if (points[j][1] > points[top_min][1] + W) {
+					top_max = j - 1;
+					break;
+				}
 			}
 		}
-		top_result = top_y / (top_count + top_max); // top 평균
-
-		int sum = 0;
-		for (int j = K- bottom_min; j < N-K+top_max; j++) { // 남북 성벽 흔적의 갯수를 알 수 없으므로 우선 남북으로 K개 만큼 빼고 나머지에 대해서 계산.
-			if (points[j][0] <= left[0] + W) sum++; // left wall
-			else if (points[j][0] >= right[0] - W) sum++;
-			else if (points[j][1] <= top_result + W && points[j][1] >= top_result - W) sum++;
-			else if (points[j][1] >= bottom_result - W && points[j][1] <= bottom_result + W) sum++;
+		else {
+			top_max = top_min;
 		}
 
-		cout << ((sum == N - K) ? "YES" : "NO") << endl;
+		int k_sum = (bottom_min) + (N - top_max - 1);
+		if (k_sum == K) { //y 조건은 충족하였으므로, x 조건에 대해서 만족하는지만 확인, 내부 사각형에 포함여부만 확인
+			for (int j = bottom_min; j <= top_max; j++) {
+				if (points[j][1] > points[bottom_max][1] && points[j][1] < points[top_min][1]) {
+					if (points[j][0] > left[0] + W && points[j][0] < right[0] - W) {
+						goto PRINT_NO;
+						break;
+					}
+				}
+			}
+
+			goto PRINT_YES;
+		}
+		else {
+			goto PRINT_NO;
+		}
+
+	PRINT_YES : 
+		cout << "YES" << endl;
+		output << "YES\n";
+		continue;
+
+	PRINT_NO:
+		cout << "NO" << endl;		
+		output << "NO\n";
 	}
 
 	input.close();
+	output.close();
 	return 0;
 }
---
+++ 2015/main.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <array>
+#include <time.h>
+
+using namespace std;
+
+int start(const string& input_file_name, const string& output_file_name);
+
+int main() {
+
+	string input_file_name = "problem2.in.test.txt";
+	string solution_file_name = "problem2.out.test.txt";
+	string output_file_name = "problem2.out.txt";
+	string result_file_name = "problem2.result.txt";
+
+	clock_t begin, end;
+	begin = clock();        // 시간설정
+	start(input_file_name, output_file_name);
+	end = clock();          // 시간설정
+	//cout << "수행시간 : " << ((end - begin) / CLOCKS_PER_SEC) << endl;
+
+	std::ifstream solution_if(solution_file_name);
+	std::ifstream output_if(output_file_name);
+	std::ofstream result_of(result_file_name);
+
+	vector<string> src;
+	vector<string> dst;	
+
+	while (!solution_if.eof()) {
+		string str;
+		solution_if >> str;
+		src.emplace_back(str);
+	}
+	solution_if.close();
+
+	while (!output_if.eof()) {
+		string str;
+		output_if >> str;
+		dst.emplace_back(str);
+	}
+	output_if.close();
+	
+	int wrong_count = 0;	
+	if (src.size() == dst.size()) {
+		for (int i = 0; i < src.size(); i++) {
+			if (src[i] == dst[i]) {
+				result_of << i+1 << " CORRECT : " << src[i] << " " << dst[i] << "\n";
+			}
+			else {
+				result_of << i + 1 << " WRONG : " << src[i] << " " << dst[i] << "\n";
+				wrong_count++;
+			}
+		}			
+	}
+	else {
+		cout << "Failed, Invalid Results, try again" << endl;
+		wrong_count++;
+	}
+	cout << "============================================================" << endl;
+	cout << "Results : " << ((wrong_count > 0) ? "Failed" : "Success") << " [COUNT : " << wrong_count << "/" << src.size()-1 << ", Elasped Time : " << (end - begin) << "]" << endl;
+	cout << "============================================================" << endl;
+			
+	result_of.close();
+	return 0;
+}
